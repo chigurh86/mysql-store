ace5fb3476bd31c3f4a7aba6ed56692769535b4b
Antonio Caos Jr <antoniocaosjr.@Antonios-MacBook-Pro.local>
2017-04-18 23:07:30 -0400

    added validation

--- bamazon.js
+++ bamazon.js
@@ -33,10 +33,20 @@ var start = function() {
 	 if (err) throw err;
 console.log("");
 		inquirer.prompt([{
-			name: "prodid",
+			  name: "prodid",
 		    type: "input",
-		    message: "What is the ID of the item you would like to buy?"
-		}]).then(function(answer){
+		    message: "What is the ID of the item you would like to buy?",
+        validate: function(value){
+                if (value === NaN) {
+                    console.log("");
+                    console.log('You cannot purchase an item without entering the item_id');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+		]).then(function(answer){
 			connection.query("SELECT * FROM products WHERE ?", {id: answer.prodid},
             function(err, res) {
             	for (i=0; i < res.length; i++){
@@ -46,19 +56,41 @@ console.log("");
       inquirer.prompt([{
 			  name: "units",
 		    type: "input",
-		    message: "How many units would you like to buy?"
-			}]).then(function(howmany){
-				if (howmany.units < res[0].stock_quantity) {
+		    message: "How many units would you like to buy?",
+        validate: function(value){
+                if (value === NaN) {
+                    console.log(value);
+                    console.log("");
+                    console.log('You cannot purchase an item without entering the item_id');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+			]).then(function(howmany){
+				if (howmany.units <= res[0].stock_quantity) {
           var newAmount = res[0].stock_quantity - howmany.units;
 					connection.query("UPDATE products SET ? WHERE ?", [{
             stock_quantity: newAmount
           },{
             id:answer.prodid
-          }], function(err, res) {
-            console.log("debug: " + res.affectedRows);
-          });
-					console.log("You purchased " + res[0].product_name + "s");
-          console.log("There are " + newAmount + " left.");
+          }], function(err, res) {});
+          var productName = res[0].product_name
+          var postChecked = productName.charAt(productName.length - 1)
+          if (postChecked != "s") {
+            console.log("You purchased " + res[0].product_name + "s");
+          }
+          else{
+            console.log("You purchased " + res[0].product_name);
+          }
+          console.log("Total Cost was " + "$" + (howmany.units * res[0].price));
+          if (newAmount > 1) {
+            console.log("There are " + newAmount + " left.");
+          }
+          else{
+            console.log("There is " + newAmount + " left.");
+          }
           start();
 				}
 				else{
